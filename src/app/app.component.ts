09bf932cdb5a5009d1e924c71223decbcec52008
Mike Huang <wellwind@mail2000.com.tw>
2023-05-08 14:30:16 +0800

    任務02：現有模板切版，及元件規劃

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    ArticlesComponent,
+    TagsComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-basic-realworld-standalone';
