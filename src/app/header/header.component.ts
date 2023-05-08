900b1d22be91bcf884a7f0b92e924dc8f747569d
Mike Huang <wellwind@mail2000.com.tw>
2023-05-08 14:56:10 +0800

    任務06：使用雙向繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  keyword = '';
 
   search(event: MouseEvent) {
     console.log(event);
