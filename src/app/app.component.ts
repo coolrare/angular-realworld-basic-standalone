f05313fad2681736849683fba23bc936cf0d923f
Mike Huang <wellwind@mail2000.com.tw>
2023-05-08 15:24:32 +0800

    任務11：自訂服務元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { TagsComponent } from './tags/tags.component';
 import { FooterComponent } from './footer/footer.component';
+import { ArticleService } from './article.service';
 
 @Component({
   selector: 'app-root',
@@ -19,72 +20,16 @@ import { FooterComponent } from './footer/footer.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  articleService = inject(ArticleService);
+
   title = 'conduit';
   subtitle = 'A place to share your <u>knowledge</u>.';
 
-  list = [
-    {
-      title:
-        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-      slug: 'zp7yqc',
-      body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
-      createdAt: '2018-05-11T21:58:27.358Z',
-      updatedAt: '2018-05-11T21:58:27.358Z',
-      tagList: [],
-      description:
-        'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
-      author: {
-        username: 'Eliseo@gardner.biz',
-        bio: 'Eliseo',
-        image: 'http://placekitten.com/200/300',
-        following: false,
-      },
-      favorited: false,
-      favoritesCount: 1,
-    },
-    {
-      title: 'quo vero reiciendis velit similique earum',
-      slug: 'p3vcsw',
-      body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
-      createdAt: '2018-05-11T21:55:00.722Z',
-      updatedAt: '2018-05-11T21:55:00.722Z',
-      tagList: [],
-      description:
-        'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
-      author: {
-        username: 'Jayne_Kuhic@sydney.com',
-        bio: 'Jayne_Kuhic',
-        image: 'http://placekitten.com/200/300',
-        following: false,
-      },
-      favorited: false,
-      favoritesCount: 3,
-    },
-    {
-      title: 'odio adipisci rerum aut animi',
-      slug: '3c69lg',
-      body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
-      createdAt: '2018-05-11T19:49:35.917Z',
-      updatedAt: '2018-05-11T19:49:35.917Z',
-      tagList: [],
-      description:
-        'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
-      author: {
-        username: 'Lew@alysha.tv',
-        bio: 'Lew',
-        image: 'http://placekitten.com/200/300',
-        following: false,
-      },
-      favorited: false,
-      favoritesCount: 5,
-    },
-  ];
-  originalList = this.list;
+  // 預設為全部文章
+  list = this.articleService.searchArticles('');
 
+  // 根據關鍵字搜尋文章
   searchArticles(keyword: string) {
-    if (!keyword) {
-      this.list = this.originalList;
-    }
-    this.list = this.originalList.filter(item => item.title.includes(keyword));
+    this.list = this.articleService.searchArticles(keyword);
   }
 }
